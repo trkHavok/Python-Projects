0b41a3af5e178941fd92f205a797d3ec0c7a82b7
Tareek Solomon <62904326+trkHavok@users.noreply.github.com>
2021-12-20 11:31:57 -0500

    Add files via upload
    Calculates the average grade for a class.

---
+++ ClassAverage.py
@@ -0,0 +1,8 @@
+scores = (10, 5, 8, 7, 9)
+quizzes = len(scores)             
+average = sum(scores) / quizzes
+print('The number of quizzes taken is:', quizzes)
+print('The scores for the quizzes are:', scores)
+print('The average score for the quizzes are:', average)
+
+
