3ddb521cf6f0929f2ba9c5776c5ba282626e5343
Tareek Solomon <62904326+trkHavok@users.noreply.github.com>
2021-12-20 11:37:33 -0500

    Add files via upload
    Program that calculates the sum of the user number inputs. Program was used to experiment with formatting in Python

---
+++ Cal.py
@@ -0,0 +1,12 @@
+first_num = int(input("Enter first number:"))
+second_num = int(input("Enter second number:"))
+third_num = int(input("Enter third number:"))
+cal_line = "+ --------"
+sum_ofnum = sum([first_num, second_num], third_num)
+
+print(f"{first_num: 10d}")
+print(f"{second_num: 10d}")
+print(f"{third_num: 10d}")
+print(f"{cal_line}")
+print(f"{sum_ofnum: 10d}")
+
