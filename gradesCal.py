bdae294e98055adac4ffd8d873b16f58aec98677
Tareek Solomon <62904326+trkHavok@users.noreply.github.com>
2021-12-20 11:50:38 -0500

    Add files via upload
    Another grades program, however this time we implemented some new functions

---
+++ gradesCal.py
@@ -0,0 +1,7 @@
+grades = [90,74,88,91]
+
+print('Your test scores are:', grades)
+print('Your best test score is:', max(grades))
+print('Your worst test score was:', min(grades))
+class_avg = sum(grades) / len(grades)
+print('Your class average is:', float(class_avg))
