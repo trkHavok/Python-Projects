ce9c1f38f3c6031f0b67f9de9f880df915bd70ed
Tareek Solomon <62904326+trkHavok@users.noreply.github.com>
2021-12-20 11:11:25 -0500

    Add files via upload

---
+++ pizzaIS612.py
@@ -0,0 +1,14 @@
+num_pizza = input()
+num_people = input()
+num_slices = input()
+
+num_slices = int(num_pizza) * int(num_slices)
+slice_people = int(num_slices) / int(num_people)
+
+
+
+print('we have', num_pizza, 'pizzas and', num_people, 'people')
+print('that means', slice_people, 'slices per teenager')
+
+
+
