54430345b63b5345266e257661d619b70972ed27
Tareek Solomon <62904326+trkHavok@users.noreply.github.com>
2021-12-21 10:46:31 -0500

    Add files via upload
    Create a rectangle using "*" based off user input.

---
+++ rectanglePrint.py
@@ -0,0 +1,8 @@
+num_rows = int(input("How many rows for rectngle?" ))
+num_cols = int(input("How many columns for rectangle?" ))
+
+
+for i in range(num_rows):
+    for j in range(num_cols):
+        print('*', end=' ')
+    print()
